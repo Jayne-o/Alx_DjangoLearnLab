035c0eaebf8f713550900a3e28a2cf092d1edc40
Jayne-o <66448516+Jayne-o@users.noreply.github.com>
2025-11-02 00:27:17 -0600

    Update manage.py

--- Introduction_to_Django/manage.py
+++ Introduction_to_Django/manage.py
@@ -1,1 +1,7 @@
+django-admin startproject LibraryProject
+cd LibraryProject
+echo. > README.md
+python manage.py runserver
+
+## go to http://127.0.0.1:8000/ to view the default Django welcome page
 
